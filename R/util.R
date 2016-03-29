e4c457869cb3031cd916f867c56f43194b973fee
Simon Urbanek <simon.urbanek@R-project.org>
2016-03-29 18:02:27 -0400

    open() now uses -cat for binary and -text for text mode

--- R/util.R
+++ R/util.R
@@ -8,11 +8,12 @@ open.HDFSpath <- function(con, open = "r", ...) {
   if (length(grep("+", open, fixed=TRUE))) stop("`open' can only be either read or write but not both")
   if (length(grep("^a", open))) stop("sorry, appending is currently unsupported")
   if (length(grep("^r", open))) {
+    fscmd <- if (length(grep("b$", open))) "-cat" else "-text"
     ## try to be friendly and for directories cat the contents
     if (system(paste(hcmd,"fs -test -d",shQuote(con))) == 0)
-      pipe(paste(hcmd,"fs -cat",shQuote(paste0(con, "/*"))), open=open)
+      pipe(paste(hcmd,"fs",fscmd,shQuote(paste0(con, "/*"))), open=open)
     else
-      pipe(paste(hcmd,"fs -cat",shQuote(con)), open=open)
+      pipe(paste(hcmd,"fs",fscmd,shQuote(con)), open=open)
   } else if (length(grep("^w", open)))
     pipe(paste(hcmd,"fs -put -", shQuote(con)), open=open)
   else
