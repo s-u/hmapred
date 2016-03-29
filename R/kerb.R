2d4f300d606c122265a0fa709427fae4c7392969
Simon Urbanek <simon.urbanek@R-project.org>
2016-03-29 19:16:02 -0400

    add force= flag to ktinit()

--- R/kerb.R
+++ R/kerb.R
@@ -1,4 +1,6 @@
-ktinit <- function(keytab="~/.keytab", user=Sys.getenv("USER"), realm=getOption("kerberos.realm")) {
+ktinit <- function(keytab="~/.keytab", user=Sys.getenv("USER"), realm=getOption("kerberos.realm"), force=FALSE) {
+  if (!force && system("klist",ignore.stdout=T, ignore.stderr=T) == 0)
+     return(invisible(TRUE))
   if (!file.exists(keytab))
     stop("Kerberos keytab file `", keytab, "' doesn't exist!")
   if (!is.null(realm)) user <- paste0(user, "@", realm)
